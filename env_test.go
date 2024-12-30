42c085673f046f3eaff20654310005cc25c531bb
Poted <o.jpkmc@gmail.com>
2024-12-30 22:30:07 +0100

    test: test
    tests with sample .env file

---
+++ env_test.go
@@ -0,0 +1,25 @@
+package env
+
+import (
+	"os"
+	"testing"
+)
+
+func TestGetEnv(t *testing.T) {
+
+	err := LoadEnv(".env")
+	if err != nil {
+		t.Error(err)
+	}
+
+	firstValue := os.Getenv("FIRST_VALUE")
+	if firstValue != "10101001" {
+		t.Error("cannot assert first value")
+	}
+
+	secondValue := os.Getenv("SECOND_VALUE")
+	if secondValue != "0n3hndr3ds3xtYSX" {
+		t.Error("cannot assert second value")
+	}
+
+}
